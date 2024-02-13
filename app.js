a38affd00e50296b64c58f1abc529e3acde36994
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-02-13 03:43:42 +0300

    add clock hands and style

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const digitalClock = document.querySelector(".digital-clock");
 const date = document.querySelector(".date");
 
+function updateClock(time) {
+  const timeNow = time.split(" ")[0].split(":");
+
+  const seconds = timeNow[2];
+  const mins = timeNow[1];
+  const hour = timeNow[0];
+
+  const secondsDegrees = (seconds / 60) * 360;
+  document.querySelector(
+    ".seconds"
+  ).style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6;
+  document.querySelector(
+    ".minutes"
+  ).style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30;
+  document.querySelector(
+    ".hours"
+  ).style.transform = `rotate(${hourDegrees}deg)`;
+}
+
 function updateTime() {
   const timeNow = new Date();
   const dateNow = new Date().toISOString();
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const timeString = timeNow.toLocaleTimeString("en-US", { timeZone });
   digitalClock.textContent = timeString;
+  updateClock(timeString);
   date.textContent = formatDate(dateNow);
 }
 
